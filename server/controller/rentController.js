0f96df0ae5ac1704ea55aa282d9c9d206b861ce4
FaizurRahuman1 <faizurrahuman70@gmail.com>
2023-06-25 19:18:23 +0530

    updated all

--- server/controller/rentController.js
+++ server/controller/rentController.js
@@ -1,10 +1,25 @@
 const Rent = require('../model/rentModel')
+const User = require('../model/userModel')
+const Book = require('../model/bookModel')
 
 const rentController = {
     getAll: async (req,res) => {
         try {
             const data = await Rent.find({})
             res.status(200).json({ length: data.length, rents: data})
+        } catch (err) {
+            return res.status(500).json({ msg: err.message })
+        }
+    },
+    getByUser: async (req,res) => {
+        try {
+            let userId = req.params.userId
+
+            const data = await Rent.find({})
+
+             let userRent = data.filter((item) => item.userId == userId)
+
+            res.status(200).json({ length: userRent.length, rents: userRent})
         } catch (err) {
             return res.status(500).json({ msg: err.message })
         }
@@ -27,7 +42,28 @@ const rentController = {
                 if(extRent)
                     return res.status(400).json({ msg: `Already you have rented a book..`})
 
-            const data =  await Rent.create(req.body)
+                // read book info
+                    const book = await Book.findById({ _id: req.body.bookId })
+                         if(!book) 
+                            return res.status(404).json({ msg: `Book details not found`})
+                // read user info
+                    const user = await User.findById({ _id: req.body.userId })
+                        if(!user)
+                            return res.status(404).json({ msg: `User details not found`})
+
+                    // update the number of copy and rented copies
+                    if(book.numberOfCopy === 0) {
+                            return res.status(400).json({ msg: "No More copies available for rent."})
+                    } 
+
+                    let newRent = {
+                            ...req.body,
+                            book,
+                            user
+                    }
+
+            const data =  await Rent.create(newRent)
+            
                 return res.status(200).json({ msg: `Rent details added successfully`, rent: data })
         } catch (err) {
             return res.status(500).json({ msg: err.message })
@@ -43,7 +79,8 @@ const rentController = {
 
                 if(extRent.bookId === req.body.bookId && extRent.userId === req.body.userId)
                     return res.status(400).json({ msg: `Already you have rented a book..`})
-                
+
+               
                 await Rent.findByIdAndUpdate({ _id: id }, {
                     amount: req.body.amount,
                     returnDate: req.body.returnDate,
@@ -56,12 +93,21 @@ const rentController = {
     },
     delete: async (req,res) => {
         try {
-            let id = req.params.id 
+            let id = req.params.id
+            let bookId = req.params.bookId
             
            let extRent = await Rent.findById({ _id: id })
                 if(!extRent) 
                     return res.status(404).json({ msg: `Requested Rent id not found`})
-            
+
+            let book = await Book.findById({_id: bookId })
+               if(book) {
+                    let rCopies = book.rentedCopies - 1
+                    let nCopy = book.numberOfCopy + 1
+
+                    await Book.findByIdAndUpdate({_id: bookId }, { rentedCopies: rCopies, numberOfCopy : nCopy })
+               }
+
                 await Rent.findByIdAndDelete({ _id: id })
                     return res.status(200).json({ msg: `Rent details deleted Successfully.`})
         } catch (err) {
--- server/route/rentRoute.js
+++ server/route/rentRoute.js
@@ -5,11 +5,14 @@ const adminAuth = require('../middleware/adminAuth')
 
 
 rentRoute.get(`/all`, authMiddleware, rentController.getAll)
+
+rentRoute.get(`/books/:userId`, authMiddleware, rentController.getByUser)
+
 rentRoute.get(`/single/:id`, authMiddleware, rentController.getSingle)
 
 rentRoute.post(`/create`, authMiddleware, adminAuth, rentController.create)
 rentRoute.patch(`/update/:id`, authMiddleware, adminAuth, rentController.update)
-rentRoute.delete(`/delete/:id`, authMiddleware,adminAuth, rentController.delete)
+rentRoute.delete(`/delete/:id/book/:bookId`, authMiddleware,adminAuth, rentController.delete)
 
 
 module.exports = rentRoute
